e1166979ebc3ac52d36073da36bd8a90e0ce4c83
patw <patw.hi@gmail.com>
2023-12-19 13:21:12 +0800

    PoC

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen(props) {
+  const handlePress = () => {
+    props.navigation.navigate('DetailModal', { screen: 'Detail2', initial: false });
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <TouchableOpacity onPress={handlePress}>
+        <Text style={{ color: 'blue' }}>Navigate to Detail2</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const Detail1Screen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Detail1 Screen</Text>
+    </View>
+  );
+};
+
+const Detail2Screen = (props) => {
+  React.useLayoutEffect(() => {
+    props.navigation.setOptions({
+      title: 'foo',
+      headerRight: () => <Text>Bar</Text>,
+    });
+  }, [props.navigation]);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Detail2 Screen</Text>
+    </View>
+  );
+};
+
+const DetailStack = createStackNavigator();
+
+function DetailModal() {
+  return (
+    <DetailStack.Navigator>
+      <DetailStack.Screen name="Detail1" component={Detail1Screen} />
+      <DetailStack.Screen name="Detail2" component={Detail2Screen} />
+    </DetailStack.Navigator>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="DetailModal" component={DetailModal} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
